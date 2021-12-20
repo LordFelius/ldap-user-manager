de54f0964139be446e71cf438f517d24322a52d2
LordFelius <95199533+LordFelius@users.noreply.github.com>
2021-12-20 15:31:50 +0800

    Add files via upload

--- www/includes/modules.inc.php
+++ www/includes/modules.inc.php
@@ -8,14 +8,14 @@
  #admin = need to be logged in as an admin to see it
 
  $MODULES = array(
-                    'log_in'          => 'hidden_on_login',
-                    'change_password' => 'auth',
-                    'account_manager' => 'admin',
-                    'log_out'         => 'auth'
+                    'log_in'          => array('title' => '登录', 'display' => 'hidden_on_login'),
+                    'change_password' => array('title' => '修改密码', 'display' => 'auth'),
+                    'account_manager' => array('title' => '管理账户', 'display' => 'admin'),
+                    'log_out'         => array('title' => '登出', 'display' => 'auth')
                   );
 
 if ($ACCOUNT_REQUESTS_ENABLED == TRUE) {
-  $MODULES['request_account'] = 'hidden_on_login';
+  $MODULES['request_account'] = array('title' => '申请账户', 'display' => 'hidden_on_login');
 }
 
 ?>
