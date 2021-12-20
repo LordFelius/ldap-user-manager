0f399e54000a015048fadd984fe86a7539ae4626
LordFelius <95199533+LordFelius@users.noreply.github.com>
2021-12-20 16:10:40 +0800

    translated

--- www/change_password/index.php
+++ www/change_password/index.php
@@ -9,45 +9,45 @@ set_page_access("user");
 
 if (isset($_POST['change_password'])) {
 
- if ((!is_numeric($_POST['pass_score']) or $_POST['pass_score'] < 3) and $ACCEPT_WEAK_PASSWORDS != TRUE) { $not_strong_enough = 1; }
- if (preg_match("/\"|'/",$_POST['password'])) { $invalid_chars = 1; }
- if ($_POST['password'] != $_POST['password_match']) { $mismatched = 1; }
+    if ((!is_numeric($_POST['pass_score']) or $_POST['pass_score'] < 3) and $ACCEPT_WEAK_PASSWORDS != TRUE) { $not_strong_enough = 1; }
+    if (preg_match("/\"|'/",$_POST['password'])) { $invalid_chars = 1; }
+    if ($_POST['password'] != $_POST['password_match']) { $mismatched = 1; }
 
- if (!isset($mismatched) and !isset($not_strong_enough) and !isset($invalid_chars) ) {
+    if (!isset($mismatched) and !isset($not_strong_enough) and !isset($invalid_chars) ) {
 
-  $ldap_connection = open_ldap_connection();
-  ldap_change_password($ldap_connection,$USER_ID,$_POST['password']) or die("change_ldap_password() failed.");
+        $ldap_connection = open_ldap_connection();
+        ldap_change_password($ldap_connection,$USER_ID,$_POST['password']) or die("change_ldap_password() failed.");
 
-  render_header("$ORGANISATION_NAME account manager - password changed");
-  ?>
-  <div class="alert alert-success">
-  <p class="text-center">Your password has been changed.</p>
-  </div>
-  <?php
-  render_footer();
-  exit(0);
- }
+        render_header("$ORGANISATION_NAME account manager - password changed");
+        ?>
+        <div class="alert alert-success">
+            <p class="text-center">Your password has been changed.</p>
+        </div>
+        <?php
+        render_footer();
+        exit(0);
+    }
 
 }
 
 render_header("Change your $ORGANISATION_NAME password");
 
 if (isset($not_strong_enough)) {  ?>
-<div class="alert alert-warning">
- <p class="text-center">The password wasn't strong enough.</p>
-</div>
+    <div class="alert alert-warning">
+        <p class="text-center">The password wasn't strong enough.</p>
+    </div>
 <?php }
 
 if (isset($invalid_chars)) {  ?>
-<div class="alert alert-warning">
- <p class="text-center">The password contained invalid characters.</p>
-</div>
+    <div class="alert alert-warning">
+        <p class="text-center">The password contained invalid characters.</p>
+    </div>
 <?php }
 
 if (isset($mismatched)) {  ?>
-<div class="alert alert-warning">
- <p class="text-center">The passwords didn't match.</p>
-</div>
+    <div class="alert alert-warning">
+        <p class="text-center">The passwords didn't match.</p>
+    </div>
 <?php }
 
 ?>
@@ -57,65 +57,65 @@ if (isset($mismatched)) {  ?>
 <script type="text/javascript">$(document).ready(function(){	$("#StrengthProgressBar").zxcvbnProgressBar({ passwordInput: "#password" });});</script>
 
 <div class="container">
- <div class="col-sm-6">
+    <div class="col-sm-6">
 
-  <div class="panel panel-default">
-   <div class="panel-heading text-center">Change your password</div>
+        <div class="panel panel-default">
+            <div class="panel-heading text-center">修改密码</div>
 
-   <ul class="list-group">
-    <li class="list-group-item">Use this form to change your <?php print $ORGANISATION_NAME; ?> password.  When you start typing your new password the gauge at the bottom will show its security strength.
-    Enter your password again in the <b>confirm</b> field.  If the passwords don't match then both fields will be bordered with red.</li>
-   </ul>
+            <ul class="list-group">
+                <li class="list-group-item">输入你的<?php print $ORGANISATION_NAME; ?>账户新密码，并在<b>确认密码</b>框中再次输入。
+                输入框下方会显示你的密码强度。若两次输入不匹配，输入框将变为红色。</li>
+            </ul>
 
-   <div class="panel-body text-center">
-   
-    <form class="form-horizontal" action='' method='post'>
+            <div class="panel-body text-center">
 
-     <input type='hidden' id="change_password" name="change_password">
-     <input type='hidden' id="pass_score" value="0" name="pass_score">
-     
-     <div class="form-group" id="password_div">
-      <label for="password" class="col-sm-4 control-label">Password</label>
-      <div class="col-sm-6">
-       <input type="password" class="form-control" id="password" name="password">
-      </div>
-     </div>
+                <form class="form-horizontal" action='' method='post'>
 
-     <script>
-      function check_passwords_match() {
+                    <input type='hidden' id="change_password" name="change_password">
+                    <input type='hidden' id="pass_score" value="0" name="pass_score">
 
-        if (document.getElementById('password').value != document.getElementById('confirm').value ) {
-            document.getElementById('password_div').classList.add("has-error");
-            document.getElementById('confirm_div').classList.add("has-error");
-        }
-        else {
-         document.getElementById('password_div').classList.remove("has-error");
-         document.getElementById('confirm_div').classList.remove("has-error");
-        }
-       }
-     </script>
+                    <div class="form-group" id="password_div">
+                        <label for="password" class="col-sm-4 control-label">密码</label>
+                        <div class="col-sm-6">
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
+                    </div>
 
-     <div class="form-group" id="confirm_div">
-      <label for="password" class="col-sm-4 control-label">Confirm</label>
-      <div class="col-sm-6">
-       <input type="password" class="form-control" id="confirm" name="password_match" onkeyup="check_passwords_match()">
-      </div>
-     </div>
+                    <script>
+                        function check_passwords_match() {
 
-     <div class="form-group">
-       <button type="submit" class="btn btn-default">Change password</button>
-     </div>
-     
-    </form>
+                            if (document.getElementById('password').value != document.getElementById('confirm').value ) {
+                                document.getElementById('password_div').classList.add("has-error");
+                                document.getElementById('confirm_div').classList.add("has-error");
+                            }
+                            else {
+                                document.getElementById('password_div').classList.remove("has-error");
+                                document.getElementById('confirm_div').classList.remove("has-error");
+                            }
+                        }
+                    </script>
 
-    <div class="progress">
-     <div id="StrengthProgressBar" class="progress progress-bar"></div>
+                    <div class="form-group" id="confirm_div">
+                        <label for="password" class="col-sm-4 control-label">确认密码</label>
+                        <div class="col-sm-6">
+                            <input type="password" class="form-control" id="confirm" name="password_match" onkeyup="check_passwords_match()">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default">修改密码</button>
+                    </div>
+
+                </form>
+
+                <div class="progress">
+                    <div id="StrengthProgressBar" class="progress progress-bar"></div>
+                </div>
+
+            </div>
+        </div>
+
     </div>
-
-   </div>
-  </div>
-
- </div>
 </div>
 <?php
 
